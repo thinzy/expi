8cb267253c345f65cb78c36ab87e4608faaa086a
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-23 23:12:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
 var d = new Date();
 var user = "<@userID>";
-int '1' = 'https://imgur.com/mETXc2R';
-int '2' = 'https://imgur.com/YJvo7Fb';
-int '3' = 'https://imgur.com/2l52LK4';
-int '4' = 'https://imgur.com/CYxOhyC';
-int '5' = 'https://imgur.com/pt0Kd82';
-int '6' = 'https://imgur.com/ExuGbkZ';
-int '7' = 'https://imgur.com/D9dn2yy';
-int '8' = 'https://imgur.com/gluYJeT';
-int '9' = 'https://imgur.com/8AiNyVc';
-int '10' = 'https://imgur.com/UtgK0tJ';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 

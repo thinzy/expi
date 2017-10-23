a0dd0c187252797f5187471bc66e039d1c77e774
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-23 21:24:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 });
 
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `>`
     if (message.substring(0, 1) == '>') {
@@ -19,37 +19,37 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // >ping
             case 'ping':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: ':ping_pong: Pong!'
                 });
             break;
 			case 'about':
-				bot.sendMessage({
+				client.sendMessage({
 					to:	channelID,
 					message: '***I am the Expedition Bot for Kilimanjaro, I shall be a robot guide assisting you all! Created by Thinzy***'
 				});
 			break;
 			case 'play':
-				bot.sendMessage({
+				client.sendMessage({
 					to: channelID,
 					message: 'https://www.roblox.com/games/1008098480/Mount-Kilimanjaro'
 				});
 			break;
 			case 'group':
-				bot.sendMessage({
+				client.sendMessage({
 					to:	channelID,
 					message: 'https://www.roblox.com/My/Groups.aspx?gid=3457860'
 				});
 			break;	
 			case 'date':
-				bot.sendMessage({
+				client.sendMessage({
 					to: channelID,
 					message: `the date is ${d}`
 				});
 			break;
 			case 'tussle':
-				bot.sendMessage({
+				client.sendMessage({
 					to: channelID,
 					message: `you wanna tussle, ${user}?`
 				});

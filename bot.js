79d0d1cf1d340988a6bfa77f7227335a88e971f5
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-23 21:03:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,7 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
 var d = new Date();
 var user = "<@userID>";
 client.login(process.env.BOT_TOKEN);
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');

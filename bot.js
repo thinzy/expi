2eb974a2477bfde717ee217d04616cc7a8f5fc05
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-23 21:30:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,54 +7,13 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === '>ping') {
+    	message.reply(':ping_pong: pong!');
+  	}
+client.on('message', message => {
+    if (message.content === '>tussle') {
+    	message.reply(`ya wanna tussle, ${user}?`);
+  	}
 
-client.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `>`
-    if (message.substring(0, 1) == '>') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // >ping
-            case 'ping':
-                client.sendMessage({
-                    to: channelID,
-                    message: ':ping_pong: Pong!'
-                });
-            break;
-			case 'about':
-				client.sendMessage({
-					to:	channelID,
-					message: '***I am the Expedition Bot for Kilimanjaro, I shall be a robot guide assisting you all! Created by Thinzy***'
-				});
-			break;
-			case 'play':
-				client.sendMessage({
-					to: channelID,
-					message: 'https://www.roblox.com/games/1008098480/Mount-Kilimanjaro'
-				});
-			break;
-			case 'group':
-				client.sendMessage({
-					to:	channelID,
-					message: 'https://www.roblox.com/My/Groups.aspx?gid=3457860'
-				});
-			break;	
-			case 'date':
-				client.sendMessage({
-					to: channelID,
-					message: `the date is ${d}`
-				});
-			break;
-			case 'tussle':
-				client.sendMessage({
-					to: channelID,
-					message: `you wanna tussle, ${user}?`
-				});
-            // Case commands go here
-         }
-     }
-});
 client.login(process.env.BOT_TOKEN);

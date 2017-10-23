751f838afd82d24cbabf1144272d336e8e86dfe1
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-23 21:08:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,6 @@ var user = "<@userID>";
 client.login(process.env.BOT_TOKEN);
 
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `>`

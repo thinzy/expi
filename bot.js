8f43825b73ef47d78be899f79fd5df33c0b66118
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-24 14:12:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
       message.reply(`${images[Math.floor(Math.random() * (images.length))]}`);
     }
     if (message.content === '>climbers') {
-      var users = [client.members.get("id", 11238414)];
+      var users = [client.users.get("id", 11238414)];
       message.reply(`${users[users.length]}`);
     }
     

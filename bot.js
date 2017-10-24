4af3656ae87c1fd31368a71463a52a5bb8c9cb53
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-24 16:00:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,9 +13,8 @@ console.log(images.length);
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on("message", async message => {
+client.on('message', (async message) => {
   if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 });

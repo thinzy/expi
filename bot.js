98e48c65ae925ae12007e8a7632aeda756e33ffb
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-24 01:01:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,39 +6,26 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Ready for expeditions!');
-});
-
-client.on('ready', () => {
-  client.user.setGame('Climbing a mountain')
+  client.user.setGame('Climbing a mountain');
+  console.log('Ready for expeditions!');
 })
 
 client.on('message', message => {
     if (message.content === '>ping') {
     	message.reply(':ping_pong: pong!');
   	}
-});
-client.on('message', message => {
     if (message.content === '>tussle') {
     	message.reply('ya wanna tussle?');
   	}
-});
-client.on('message', message => {
     if (message.content === '>play') {
     	message.reply('https://www.roblox.com/games/1008098480/Mount-Kilimanjaro');
   	}
-});
-client.on('message', message => {
     if (message.content === '>group') {
     	message.reply('https://www.roblox.com/My/Groups.aspx?gid=3457860');
   	}
-});
-client.on('message', message => {
     if (message.content === '>date') {
     	message.reply(`${d}`);
   	}
-});
-client.on('message', message => {
     if (message.content === '>rsummit') {
         message.reply(`${image}`);
     }

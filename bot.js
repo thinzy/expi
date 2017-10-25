1e45e15ff6c0703167705de81e285afa241b6844
thinzy <33039806+thinzy@users.noreply.github.com>
2017-10-25 23:49:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,9 +68,7 @@ client.on('message', message => {
       message.reply({embed});
     }
   if (message.content === '>rating') {
-      message.reply('Rate the bot!')
-      message.react("😍");
-      message.react("💩");
+      message.reply('Rate the bot!'{message.react("😍")}{message.react("💩")});
     }
 });
 

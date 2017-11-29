298a301b5124565bfb7d3e7ca9abab24803ae567
thinzy <33039806+thinzy@users.noreply.github.com>
2017-11-29 18:39:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,8 @@ var images = ['https://imgur.com/mETXc2R',
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ">"
+const sql = require("sqlite");
+sql.open("./score.sqlite");
 const embed = new Discord.RichEmbed()
   .setTitle("Commands List")
   .setAuthor("Expi | Expedition Bot", "https://imgur.com/qVuDedv.png")
@@ -57,6 +59,8 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.channel.type !== "text") return;
     if (message.content === '>ping') {
     	message.reply(`:ping_pong: Pong! It took ${Math.round(client.ping)}ms to climb the mountain!`);
   	}
@@ -97,6 +101,41 @@ client.on('message', message => {
     if (message.content === '>onl1n3') {
      client.channels.get('352513398904324100').send('I am coming on too, expect me to be there :D');
     }
+    
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    } else {
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      if (curLevel > row.level) {
+        row.level = curLevel;
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        message.reply(`well done!You have leveled up to **${curLevel}**! Keep on climbin'!`);
+      }
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+    }
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    });
+  });
+
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "level")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("Your current level is 0");
+      message.reply(`Your current level is ${row.level}`);
+    });
+  } else
+
+  if (message.content.startsWith(prefix + "points")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("sadly you do not have any points yet!");
+      message.reply(`you currently have ${row.points} points, good going!`);
+    });
+  }
 });
 
 
